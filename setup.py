3159a79c8c8d4dac95b20cf2d814fe5daf94bf6e
Gerben Beintema <g.i.beintema@tue.nl>
2020-11-07 23:24:28 +0100

    minor edits

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
 from setuptools import setup
 
 setup(name='deepSI',
-      version='0.1',
+      version='0.2',
+      description = 'Dynamical system identification',
+      author = 'Gerben Beintema',
+      author_email = 'g.i.beintema@tue.nl',
+      license = 'BSD 3-Clause License',
+      python_requires = '>=3.6',
       install_requires=['numpy','matplotlib', 'tqdm', 'progressbar','torch','scikit-learn', 'gym']
      )
+
+  # extras_require = dict(
+  #   docs=['Sphinx>=1.6','scipy>=0.13','matplotlib>=1.3'],
+  #   matrix_scipy=['scipy>=0.13'],
+  #   matrix_mkl=['mkl'],
+  #   export_mpl=['matplotlib>=1.3','pillow>2.6'],
+  #   import_gmsh=['meshio'],
+  # ),
+  # command_options = dict(
+  #   test=dict(test_loader=('setup.py', 'unittest:TestLoader')),
+  # ),
