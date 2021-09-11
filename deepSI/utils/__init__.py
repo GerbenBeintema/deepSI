c795f0fa03aefa20650f7786f1b6027a365f307e
Gerben Beintema <g.i.beintema@tue.nl>
2021-09-11 16:56:59 +0200

    CNN first attempt, need to fix memory issue

--- deepSI/utils/__init__.py
+++ deepSI/utils/__init__.py
@@ -1,2 +1,3 @@
-from deepSI.utils.torch_nets import simple_res_net, feed_forward_nn, affine_forward_layer, integrators_RK4
+from deepSI.utils.torch_nets import simple_res_net, feed_forward_nn, affine_forward_layer, \
+									integrators_RK4, CNN_chained_upscales, FC_video
 import deepSI.utils.sklearn_regs
--- deepSI/utils/torch_nets.py
+++ deepSI/utils/torch_nets.py
@@ -42,6 +42,175 @@ class affine_input_net(nn.Module):
         gnow = self.g_net_now(z).view(-1,self.output_dim,self.input_dim)
         return torch.einsum('nij,nj->ni', gnow, u)
 
+
+
+class ConvShuffle(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, padding='same', upscale_factor=2, padding_mode='zeros'):
+        super(ConvShuffle, self).__init__()
+        self.upscale_factor = upscale_factor
+        self.conv = nn.Conv2d(in_channels, out_channels*upscale_factor**2, kernel_size, padding=padding, padding_mode=padding_mode)
+    
+    def forward(self, X):
+        X = self.conv(X) #(N, Cout*upscale**2, H, W)
+        return nn.functional.pixel_shuffle(X, self.upscale_factor) #(N, Cin, H*r, W*r)
+
+class ShuffleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, padding='same', upscale_factor=2, padding_mode='zeros'):
+        super(ShuffleConv, self).__init__()
+        self.upscale_factor = upscale_factor
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding, padding_mode=padding_mode) #kernal larger?
+    
+    def forward(self, X):
+        X = torch.cat([X]*self.upscale_factor**2,dim=1) #(N, Cin*r**2, H, W)
+        X = nn.functional.pixel_shuffle(X, self.upscale_factor)  #(N, Cin, H*r, W*r)
+        return self.conv(X)
+        
+class ClassicUpConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, padding='same', upscale_factor=2, padding_mode='zeros'):
+        super(ClassicUpConv, self).__init__()
+        self.upscale_factor = upscale_factor
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding, padding_mode=padding_mode) #kernal larger?
+        self.up = nn.Upsample(size=None,scale_factor=upscale_factor,mode='bicubic',align_corners=False)
+
+    def forward(self, X):
+        X = self.up(X) #(N, Cin, H*r, W*r)
+        return self.conv(X)
+
+
+#todo:
+# class ConvTranspose(nn.Module):
+#     def __init__(self, in_channels, out_channels, kernel_size, padding='same', upscale_factor=2, padding_mode='replicate'):
+#         super(ConvTranspose, self).__init__()
+#         self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride=upscale_factor, padding=padding, padding_mode='zeros')
+
+#     def forward(self, X):
+#         print(X.shape)
+#         X = self.conv(X) #padding is weird
+#         print(X.shape)
+#         return X #(N, Cin, H*r, W*r)
+
+#todo figure out valid padding and compare to zero padding implemention on cnntesting project.
+
+class Upscale_Conv_block(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, padding='same', \
+                 upscale_factor=2, main_upscale=ConvShuffle, shortcut=ConvShuffle, \
+                 padding_mode='zeros', activation=nn.functional.relu, Ch=0, Cw=0):
+        assert isinstance(upscale_factor, int)
+        super(Upscale_Conv_block, self).__init__()
+        #padding='valid' is weird????
+        self.shortcut = shortcut(in_channels, out_channels, kernel_size, padding=padding, padding_mode=padding_mode, upscale_factor=upscale_factor)
+        self.activation = activation
+        self.upscale = main_upscale(in_channels, out_channels, kernel_size, padding=padding, padding_mode=padding_mode, upscale_factor=upscale_factor)
+        self.conv = nn.Conv2d(out_channels, out_channels, kernel_size, padding=padding, padding_mode=padding_mode)
+        self.Ch = Ch
+        self.Cw = Cw
+        
+    def forward(self, X):
+        #shortcut
+        X_shortcut = self.shortcut(X) # (N, Cout, H*r, W*r)
+        
+        #main line
+        X = self.activation(X) # (N, Cin, H, W)
+        X = self.upscale(X)    # (N, Cout, H*r, W*r)
+        X = self.activation(X) # (N, Cout, H*r, W*r)
+        X = self.conv(X)       # (N, Cout, H*r, W*r)
+        
+        #combine
+        X = X + X_shortcut
+        return X[:,:,self.Ch:,self.Cw:] #slice if needed
+
+
+class CNN_chained_upscales(nn.Module):
+    def __init__(self, nx, ny, features_out = 1, kernel_size=3, padding='same', \
+                 upscale_factor=2, main_upscale=ConvShuffle, shortcut=ConvShuffle, \
+                 padding_mode='zeros', activation=nn.functional.relu):
+
+        super(CNN_chained_upscales, self).__init__()
+        self.activation  = activation
+        assert isinstance(ny,(list,tuple)) and (len(ny)==2 or len(ny)==3), 'ny should have 2 or 3 dimentions in the form (nchannels, height, width) or (height, width)'
+        if len(ny)==2:
+            self.nchannels = 1
+            self.None_nchannels = True
+            self.height_target, self.width_target = ny
+        else:
+            self.None_nchannels = False
+            self.nchannels, self.height_target, self.width_target = ny
+        
+        #work backwards
+        height_now = self.height_target
+        width_now = self.width_target
+        features_now = features_out
+        
+        self.upblocks = []
+        while height_now>=2*upscale_factor+1 and width_now>=2*upscale_factor+1:
+            
+            Ch = (-height_now)%upscale_factor
+            Cw = (-width_now)%upscale_factor
+            # print(height_now, width_now, features_now, Ch, Cw)
+            B = Upscale_Conv_block(features_now*upscale_factor, features_now, kernel_size, padding=padding, \
+                 upscale_factor=upscale_factor, main_upscale=main_upscale, shortcut=shortcut, \
+                 padding_mode=padding_mode, activation=activation, Cw=Cw, Ch=Ch)
+            self.upblocks.append(B)
+            features_now *= upscale_factor
+            #implement slicing 
+            
+            height_now += Ch
+            width_now += Cw
+            height_now //= upscale_factor
+            width_now //= upscale_factor
+        # print(height_now, width_now, features_now)
+        self.width0 = width_now
+        self.height0 = height_now
+        self.features0 = features_now
+        
+        self.upblocks = nn.Sequential(*list(reversed(self.upblocks)))
+        self.FC = simple_res_net(n_in=nx,n_out=self.width0*self.height0*self.features0, n_hidden_layers=1)
+        self.final_conv = nn.Conv2d(features_out, self.nchannels, kernel_size=3, padding=padding, padding_mode='zeros')
+        
+    def forward(self, x):
+        X = self.FC(x).view(-1, self.features0, self.height0, self.width0) 
+        X = self.upblocks(X)
+        X = self.activation(X)
+        Xout = self.final_conv(X)
+        return Xout[:,0,:,:] if self.None_nchannels else Xout
+
+class FC_video(nn.Module):
+    def __init__(self, nx, ny, n_nodes_per_layer=64, n_hidden_layers=2, activation=nn.Tanh):
+        super(FC_video, self).__init__()
+        assert isinstance(ny,(list,tuple)) and (len(ny)==2 or len(ny)==3), 'ny should have 2 or 3 dimentions in the form (nchannels, height, width) or (height, width)'
+        if len(ny)==2:
+            self.nchannels = 1
+            self.None_nchannels = True
+            self.height_target, self.width_target = ny
+        else:
+            self.None_nchannels = False
+            self.nchannels, self.height_target, self.width_target = ny
+
+        self.FC = simple_res_net(n_in=nx, n_out=self.nchannels*self.height_target*self.width_target, \
+                  n_hidden_layers=n_hidden_layers, activation=activation, n_nodes_per_layer=n_nodes_per_layer)
+
+    def forward(self,x):
+        Xout = self.FC(x).view(-1, self.nchannels, self.height_target, self.width_target)
+        return Xout[:,0,:,:] if self.None_nchannels else Xout
+
+if __name__ == '__main__':
+    ny = (20,20)
+    nx = 8
+    upscale_factor = 2
+    features_out = 2
+    for up in [ConvShuffle, ShuffleConv, ClassicUpConv]:
+        padding_mode='replicate' #or zero
+        activation = nn.functional.relu # or torch.tanh
+        f = CNN_chained_upscales(nx, ny, features_out=features_out, \
+                             shortcut=up, main_upscale=up, padding_mode=padding_mode, \
+                            activation=activation, upscale_factor=upscale_factor)
+        print(f(torch.randn(1,nx)).shape)
+
+    f = FC_video(nx=nx, ny=ny)
+    print(f(torch.randn(1,nx)).shape)
+
+
+
 class affine_forward_layer(nn.Module):
     """
     Implantation of
