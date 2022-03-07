2cbcabe7b7955fc241dea6ed9222af79765c1c01
seaveredge <50490905+seaveredge@users.noreply.github.com>
2022-03-07 11:49:45 +0100

    Typos

--- deepSI/fit_systems/__init__.py
+++ deepSI/fit_systems/__init__.py
@@ -2,7 +2,7 @@ from deepSI.fit_systems.fit_system import System_fittable, System_torch
 from deepSI.fit_systems.hyperparameter_optimization import random_search, grid_search
 from deepSI.fit_systems.sklearn_io import Sklearn_io, Sklearn_io_linear
 from deepSI.fit_systems.encoders import SS_encoder, SS_encoder_general, \
-										SS_par_start, SS_encoder_general_koopman, SS_encoder_inovation,\
+										SS_par_start, SS_encoder_general_koopman, SS_encoder_innovation,\
 										SS_encoder_CNN_video, SS_encoder_shotgun_MLP, SS_encoder_deriv_general, SS_encoder_general_hf
 from deepSI.fit_systems.torch_io import Torch_io_siso, Torch_io
 from deepSI.fit_systems.ss_linear import SS_linear
